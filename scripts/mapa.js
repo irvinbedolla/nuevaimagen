e67bef7fabf5e18c803b9ce17bd1995a2b65e3f3
Artemisa <1825812h@umich.mx>
2026-05-04 16:11:35 -0600

    Se agrego el mapa de Michoacán, con cada uno de los municipios divididos por regiones. Cada region corresponde a las sedes de el centro de conciliacion en el estado(Morelia, Zitacuaro, Lazaro Cardenas, Uruapan y Sahuayo), ademas, cada una de estas sedes principales se le agrega un marcador para ubicarlas mas facilmente. Al momento de clickear sobre uno de los municipios principales aparece una carta que muestra información correspondiente al centro de conciliación. Para finalizar, en la parte superior se encuentran 7 botones 6 de ellos son para remarcar la region de la sede que quieres ver, el boton restante deja el mapa con todas las regiones iluminadas.

---
+++ scripts/mapa.js
@@ -0,0 +1,575 @@
+// Municipios
+const datosMunicipios = {
+    
+    "Morelia":{
+        region: "region-morelia",
+        info: "Pertenece a la Delegación Regional Morelia. Sede de apoyo Morelia."
+    },
+    "SantaAnaMaya":{
+        municipio:"Santa Ana Maya",
+        region: "region-morelia",
+    },
+    "AlvaroObregon":{
+        municipio:"Alvaro Oregon",
+        region: "region-morelia",
+    },
+    "Zinapecuaro":{
+        municipio:"Zinapecuaro",
+        region: "region-morelia",
+    },
+    "Maravatio":{
+        municipio:"Maravatio",
+        region: "region-morelia",
+    },
+    "EpitacioHuerta":{
+        municipio:"Epitacio Huerta",
+        region: "region-morelia",
+    },
+    "Contepec":{
+        municipio:"Contepec",
+        region: "region-morelia",
+    },
+    "Querendaro":{
+        municipio:"Querendaro",
+        region: "region-morelia",
+    },
+    "Tzitzio":{
+        municipio:"Tzitzio",
+        region: "region-morelia",
+    },
+    "Madero":{
+        municipio:"Madero",
+        region: "region-morelia",
+    },
+    "Acuitzio":{
+        municipio:"Acuitzio",
+        region: "region-morelia",
+    },
+    "SalvadorEscalante":{
+        municipio:"Salvador Escalante",
+        region: "region-morelia",
+    },
+    "Patzcuaro":{
+        municipio:"Patzcuaro",
+        region: "region-morelia",
+    },
+    "Erongaricuaro":{
+        municipio:"Erongaricuaro",
+        region: "region-morelia",
+    },
+    "Quiroga":{
+        municipio:"Quiroga",
+        region: "region-morelia",
+    },
+    "Chucandiro":{
+        municipio:"Chucandiro",
+        region: "region-morelia",
+    },
+    "Huandacareo":{
+        municipio:"Huandacareo",
+        region: "region-morelia",
+    },
+    "Cuitzeo":{
+        municipio:"Cuitzeo",
+        region: "region-morelia",
+    },
+    "Tzintzuntzan":{
+        municipio:"Tzintzuntzan",
+        region: "region-morelia",
+    },
+    "Lagunillas":{
+        municipio:"Lagunillas",
+        region: "region-morelia",
+    },
+    "Huiramba":{
+        municipio:"Huiramba",
+        region: "region-morelia",
+    },
+    "Copandaro":{
+        municipio:"Copandaro",
+        region: "region-morelia",
+    },
+    "Tarimbaro":{
+        municipio:"Tarimbaro",
+        region: "region-morelia",
+    },
+    "Charo":{
+        municipio:"Charo",
+        region: "region-morelia",
+    },
+    "Indaparapeo":{
+        municipio:"Indaparapeo",
+        region: "region-morelia",
+    },
+    
+    
+
+    
+    "Zitacuaro": { 
+        region: "region-zitacuaro", 
+        info: "Pertenece a la Delegación Regional Zitacuaro. Sede de apoyo Zitacuaro." 
+    },
+    "Tlalpujahua": { 
+        municipio:"Tlalpujahua",
+        region: "region-zitacuaro", 
+    },
+    "Senguio": { 
+        municipio:"Senguio",
+        region: "region-zitacuaro", 
+    },
+    "Angangueo": { 
+        municipio:"Angangueo",
+        region: "region-zitacuaro", 
+    },
+    "Ocampo": { 
+        municipio:"Ocampo",
+        region: "region-zitacuaro", 
+    },
+    "Juarez": { 
+        municipio:"Juarez",
+        region: "region-zitacuaro", 
+    },
+    "Susupuato": { 
+        municipio:"Susupuato",
+        region: "region-zitacuaro", 
+    },
+    "Tuzantla": { 
+        municipio:"Tuzantla",
+        region: "region-zitacuaro", 
+    },
+    "TiquicheoDeNicolasRomero": { 
+        municipio:"Tiquicheo De Nicolas Romero",
+        region: "region-zitacuaro", 
+    },
+    "SanLucas": { 
+        municipio:"San Lucas",
+        region: "region-zitacuaro", 
+    },
+    "Huetamo": { 
+        municipio:"Huetamo",
+        region: "region-zitacuaro", 
+    },
+    "Irimbo": { 
+        municipio:"Irimbo",
+        region: "region-zitacuaro", 
+    },
+    "Hidalgo": { 
+        municipio:"Hidalgo",
+        region: "region-zitacuaro", 
+    },
+    "Tuzantla": { 
+        municipio:"Tuzantla",
+        region: "region-zitacuaro", 
+    },
+    "Jungapeo": { 
+        municipio:"Jungapeo",
+        region: "region-zitacuaro", 
+    },
+    "Tuxpan": { 
+        municipio:"Tuxpan",
+        region: "region-zitacuaro", 
+    },
+    "Aporo": { 
+        municipio:"Aporo",
+        region: "region-zitacuaro", 
+    },
+
+
+    "Uruapan":{
+        region: "region-uruapan",
+        info: "Pertenece a la Delegación Regional Uruapan. Sede de apoyo Uruapan."
+    },
+    
+    "Tacambaro":{
+        municipio:"Tacambaro",
+        region: "region-uruapan",
+    },
+    "Cocupetaro":{
+        municipio:"cocupetaro",
+        region: "region-uruapan",
+    },
+    "Caracuaro":{
+        municipio:"Caracuaro",
+        region: "region-uruapan",
+    },
+    "Turicato":{
+        municipio:"Turicato",
+        region: "region-uruapan",
+    },
+    "Churumuco":{
+        municipio:"Churumuco",
+        region: "region-uruapan",
+    },
+    "LaHuacana":{
+        municipio:"La Huacana",
+        region: "region-uruapan",
+    },
+    "Tumbiscatio":{
+        municipio:"Tumbiscatio",
+        region: "region-uruapan",
+    },
+    "CoalcomanDeVazquezPallares":{
+        municipio:"Coalcoman De Vazquez Pallares",
+        region: "region-uruapan",
+    },
+    "Coahuayana":{
+        municipio:"Coahuayana",
+        region: "region-uruapan",
+    },
+    "Chinicuila":{
+        municipio:"Chinicuila",
+        region: "region-uruapan",
+    },
+    "Nocupetaro":{
+        municipio:"Nocupetaro",
+        region: "region-uruapan",
+    },
+    "Tepalcatepec":{
+        municipio:"Tepalcatepec",
+        region: "region-uruapan",
+    },
+    "Buenavista":{
+        municipio:"Buenavista",
+        region: "region-uruapan",
+    },
+    "Tancitaro":{
+        municipio:"Tancitaro",
+        region: "region-uruapan",
+    },
+    "NuevoParangaricutiro":{
+        municipio:"Nuevo Parangaricutiro",
+        region: "region-uruapan",
+    },
+    "Charapan":{
+        municipio:"Charapan",
+        region: "region-uruapan",
+    },
+    "Paracho":{
+        municipio:"Paracho",
+        region: "region-uruapan",
+    },
+    "Nahuatzen":{
+        municipio:"Nahuatzen",
+        region: "region-uruapan",
+    },
+    "Tingambato":{
+        municipio:"Tingambato",
+        region: "region-uruapan",
+    },
+    "Ziracuaretiro":{
+        municipio:"Ziracuaretiro",
+        region: "region-uruapan",
+    },
+    "Taretan":{
+        municipio:"Taretan",
+        region: "region-uruapan",
+    },
+    "Ario":{
+        municipio:"Ario",
+        region: "region-uruapan",
+    },
+    "NuevoUrecho":{
+        municipio:"Nuevo Urecho",
+        region: "region-uruapan",
+    },
+    "GabrielZamora":{
+        municipio:"Gabriel Zamora",
+        region: "region-uruapan",
+    },
+    "Mugica":{
+        municipio:"Mugica",
+        region: "region-uruapan",
+    },
+    "Paracuaro":{
+        municipio:"Paracuaro",
+        region: "region-uruapan",
+    },
+    "Apatzingan":{
+        municipio:"Apatzingan",
+        region: "region-uruapan",
+    },
+    "Aguililla":{
+        municipio:"Aguililla",
+        region: "region-uruapan",
+    },
+
+
+    "LazaroCardenas": { 
+        region: "region-lazaro", 
+        info: "Pertenece a la Delegación Regional Lázaro Cárdenas. Sede de apoyo Lázaro Cárdenas." 
+    },
+    "Aquila": { 
+        municipio:"Aquila",
+        region: "region-lazaro", 
+    },
+    "Arteaga": { 
+        municipio:"Arteaga",
+        region: "region-lazaro", 
+    },
+
+    "Sahuayo": { 
+        municipio:"",
+        region: "region-sahuayo", 
+        info: "Pertenece a la Delegación Regional Sahuayo. Sede de apoyo Sahuayo." 
+    },
+    "Cotija": { 
+        municipio:"Cotija",
+        region: "region-sahuayo", 
+    },
+    "Villamar": { 
+        municipio:"Villamar",
+        region: "region-sahuayo", 
+    },
+    "Pajacuaran": { 
+        municipio:"Pajacuaran",
+        region: "region-sahuayo", 
+    },
+    "VistaHermosa": {
+        municipio:"Vista Hermosa", 
+        region: "region-sahuayo", 
+    },
+    "Briseñas": { 
+        municipio:"Briseñas",
+        region: "region-sahuayo", 
+    },
+    "VenustianoCarranza": { 
+        municipio:"Venustiano Carranza",
+        region: "region-sahuayo", 
+    },
+    "Jiquilpan": { 
+        municipio:"Jiquilpan",
+        region: "region-sahuayo", 
+    },
+    "CojumatlanDeRegules": { 
+        municipio:"Cojumatlan De Regules",
+        region: "region-sahuayo", 
+    },
+    "MarcosCastellanos": { 
+        municipio:"Marcos Castellanos",
+        region: "region-sahuayo", 
+    },
+    
+    "Zamora": { 
+        region: "region-zamora", 
+        info: "Pertenece a la Delegación Regional Zamora. Sede de apoyo Zamora." 
+    },
+    "Tanhuato": { 
+        municipio:"Tanhuato",
+        region: "region-zamora", 
+    },
+    "Yurecuaro": { 
+        municipio:"Yurecuaro",
+        region: "region-zamora", 
+    },
+    "LaPiedad": { 
+        municipio:"La Piedad",
+        region: "region-zamora", 
+    },
+    "Numaran": { 
+        municipio:"Numaran",
+        region: "region-zamora", 
+    },
+    "Penjamillo": { 
+        municipio:"Penjamillo",
+        region: "region-zamora", 
+    },
+    "Angamacutiro": {
+        municipio:"Angamacutiro", 
+        region: "region-zamora", 
+    },
+    "JoseSixtoVerduzco": { 
+        municipio:"Jose Sixto Verduzco",
+        region: "region-zamora", 
+    },
+    "Puruandiro": { 
+        municipio:"Puruandiro",
+        region: "region-zamora", 
+    },
+    "Morelos": { 
+        municipio:"Morelos",
+        region: "region-zamora", 
+    },
+    "Huaniqueo": { 
+        municipio:"Huaniqueo",
+        region: "region-zamora", 
+    },
+    "Coeneo": { 
+        municipio:"Coeneo",
+        region: "region-zamora", 
+    },
+
+    "Zacapu": { 
+        municipio:"Zacapu",
+        region: "region-zamora", 
+    },
+    "Cheran": { 
+        municipio:"Cheran",
+        region: "region-zamora", 
+    },
+    "Chilchota": { 
+        municipio:"Chilchota",
+        region: "region-zamora", 
+    },
+    "Tangancicuaro": { 
+        municipio:"Tangancicuaro",
+        region: "region-zamora", 
+    },
+    "LosReyes": { 
+        municipio:"Los Reyes",
+        region: "region-zamora", 
+    },
+    "Periban": { 
+        municipio:"Periban",
+        region: "region-zamora", 
+    },
+    "Tocumbo": { 
+        municipio:"Tocumbo",
+        region: "region-zamora", 
+    },
+    "Tinguindin": { 
+        municipio:"Tinguindin",
+        region: "region-zamora", 
+    },
+    "Tangamandapio": { 
+        municipio:"Tangamandapio",
+        region: "region-zamora", 
+    },
+    "Chavinda": { 
+        municipio:"Chavinda",
+        region: "region-zamora", 
+    },
+    "Ixtlan": { 
+        municipio:"Ixtlan",
+        region: "region-zamora", 
+    },
+    "Tanhuato": { 
+        municipio:"Tanhuato",
+        region: "region-zamora", 
+    },
+    "Jacona": { 
+        municipio:"Jacona",
+        region: "region-zamora", 
+    },
+    "Panindicuaro": {
+        municipio:"Panindicuaro", 
+        region: "region-zamora", 
+    },
+    "Jimenez": { 
+        municipio:"Jimenez",
+        region: "region-zamora", 
+    },
+    "Ecuandureo": { 
+        municipio:"Ecuandureo",
+        region: "region-zamora", 
+    },
+    "Churintzio": { 
+        municipio:"Churintzio",
+        region: "region-zamora", 
+    },
+    "Tlazazalca": { 
+        municipio:"Tlazazalca",
+        region: "region-zamora", 
+    },
+    "Purepero": { 
+        municipio:"Purepero",
+        region: "region-zamora", 
+    },
+    "Zinaparo": { 
+        municipio:"Zinaparo",
+        region: "region-zamora", 
+    },
+    
+    
+
+};
+
+// Inicializar el mapa (Pintar colores y asignar eventos)
+document.addEventListener("DOMContentLoaded", () => {
+    const municipiosSvg = document.querySelectorAll("#mapa-michoacan path");
+
+    municipiosSvg.forEach(path => {
+        const id = path.id;
+        
+        // Si el municipio existe en nuestro diccionario
+        if(datosMunicipios[id]) {
+            // Le asignamos su color (clase CSS)
+            path.classList.add(datosMunicipios[id].region);
+
+            // Le asignamos el evento click para mostrar la carta
+            if(datosMunicipios[id].info) { // Solo asigna el evento si hay info para mostrar
+                path.addEventListener("click", () => {
+                    abrirCarta(id, datosMunicipios[id].info);
+                });
+            }
+        }
+    });
+});
+
+//Función para filtrar por región
+function filtrarMapa(regionFiltro) {
+    const municipiosSvg = document.querySelectorAll("#mapa-michoacan path");
+    
+    municipiosSvg.forEach(path => {
+        const id = path.id;
+        if(datosMunicipios[id]) {
+            if (regionFiltro === 'todas') {
+                path.classList.remove('opacado'); // Muestra todos
+            } else {
+                // Verifica si la región del municipio coincide con el filtro
+                if (datosMunicipios[id].region === `region-${regionFiltro}`) {
+                    path.classList.remove('opacado');
+                } else {
+                    path.classList.add('opacado'); // Apaga los demás
+                }
+            }
+        }
+    });
+    cerrarCarta(); // Ocultar carta al cambiar de filtro
+}
+
+//Funciones para la Carta/Ventana
+function abrirCarta(nombre, info) {
+    const carta = document.getElementById("carta-info");
+    const titulo = document.getElementById("municipio-nombre");
+    const descripcion = document.getElementById("municipio-datos");
+
+    titulo.textContent = nombre;
+    descripcion.textContent = info;
+    
+    carta.classList.remove("oculto");
+}
+
+function cerrarCarta() {
+    document.getElementById("carta-info").classList.add("oculto");
+}
+function nombreMunicipio(id){
+    const municipio = datosMunicipios[id];
+    return municipio ? municipio.municipio || id : id; // Devuelve el nombre del municipio o el ID si no se encuentra
+}
+document.addEventListener("DOMContentLoaded", () => {
+    const municipiosSvg = document.querySelectorAll("#mapa-michoacan path");
+    const tooltip = document.getElementById("tooltip-mapa"); // Seleccionamos el tooltip
+
+    municipiosSvg.forEach(path => {
+        const id = path.id;
+        
+        if(datosMunicipios[id]) {
+            path.classList.add(datosMunicipios[id].region);
+
+       
+            path.addEventListener("mouseenter", () => {
+                tooltip.textContent = datosMunicipios[id].municipio;
+                tooltip.classList.remove("oculto"); // Se hacevisible
+            });
+
+            path.addEventListener("mousemove", (evento) => {
+                tooltip.style.left = (evento.clientX + 15) + "px";
+                tooltip.style.top = (evento.clientY + 15) + "px";
+            });
+
+            path.addEventListener("mouseleave", () => {
+                tooltip.classList.add("oculto"); //Se oculta
+            });
+        }
+    });
+});
